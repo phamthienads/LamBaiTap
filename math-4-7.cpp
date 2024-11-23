5372405034f97ebad60c6a3cf7b90998855d8fe9
phamthienads <phamthien.ads@icloud.com>
2024-11-23 12:44:00 +0700

    Update

---
+++ math-4-7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    // Khai báo hai biến int a và b
+    int a = 4;
+    int b = 7;
+
+    // Hiển thị biểu thức số học và tính tổng
+    cout << "Biểu thức: " << a << " + " << b << endl;
+    cout << "Tổng: " << a + b << endl;
+
+    return 0;
+}
