fe9e05564e0eeb765bccb1dd5367c3f69131787c
phamthienads <phamthien.ads@icloud.com>
2024-11-23 12:59:17 +0700

    Update

---
+++ call-the-func.cpp
@@ -0,0 +1,17 @@
+// Function definition (example)
+function checkValue() {
+
+    // Function logic here
+    console.log("Checking value...");
+    // Example logic without parameters
+    let value = 10; // or any default value
+    if (value > 0) {
+        console.log("The value is positive.");
+    } else if (value < 0) {
+        console.log("The value is negative.");
+    } else {
+        console.log("The value is zero.");
+    }
+}
+// Function invocation with no parameters
+checkValue();
