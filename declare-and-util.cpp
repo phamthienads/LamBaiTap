0f1f1bf080f31564cfe22f40e5dbf3e91f1b495a
phamthienads <phamthien.ads@icloud.com>
2024-11-28 11:19:57 +0700

    Update

--- declare-and-util.cpp
+++ declare-and-util.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 
 int main() {
-    int userScore = 30;      // Khai báo biến userScore
-    int& scoreRef = userScore; // Tạo biến tham chiếu scoreRef tham chiếu đến userScore
-    // & là toán tử tham chiếu
+    int userScore = 30;
+    int& scoreRef = userScore;
 
-    // Hiển thị giá trị ban đầu
     cout << "userScore = " << userScore << ", scoreRef = " << scoreRef << endl;
 
-    // Thay đổi giá trị thông qua scoreRef
     scoreRef = 145;
 
-    // Hiển thị giá trị sau khi thay đổi
     cout << "userScore = " << userScore << ", scoreRef = " << scoreRef << endl;
 
     return 0;
